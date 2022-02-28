95d95a63298726d6e9bbfd72bdc497556d1cdded
johncraik <jgcraik1@sheffield.ac.uk>
2022-02-28 16:48:37 +0000

    Added  classes

---
+++ ChessSimple/Board.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessSimple
+{
+    internal class Board
+    {
+        private bool active;
+
+        /// <summary>
+        /// Initialise new board.
+        /// </summary>
+        /// <param name="active"></param>
+        public Board(bool active)
+        {
+            this.active = active;
+        }
+
+        /// <summary>
+        /// Returns the bool active. True when game is running, false otherwise.
+        /// </summary>
+        /// <returns></returns>
+        public bool getActive()
+        {
+            return this.active;
+        }
+
+        /// <summary>
+        /// Set the bool active. True when game is running, false otherwise.
+        /// </summary>
+        /// <param name="active"></param>
+        public void setActive(bool active)
+        {
+            this.active = active;
+        }
+    }
+}
---
+++ ChessSimple/Square.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace ChessSimple
+{
+    internal class Square : Board
+    {
+        private Canvas sqr;
+        private Point index;
+        private ChessPiece piece;
+
+        public Square(bool active, Canvas sqr, Point index, ChessPiece piece) : base(active)
+        {
+            this.sqr = sqr;
+            this.index = index;
+            this.piece = piece;
+        }
+
+        public Canvas getSquare()
+        {
+            return sqr;
+        }
+
+        public Point getIndex()
+        {
+            return index;
+        }
+
+        public int getX()
+        {
+            return (int) index.X;
+        }
+
+        public int getY()
+        {
+            return (int) index.Y;
+        }
+
+        public ChessPiece getChessPiece()
+        {
+            return piece;
+        }
+
+        public void setSquare(Canvas sqr)
+        {
+            this.sqr = sqr;
+        }
+
+        public void setIndex(Point index)
+        {
+            this.index = index;
+        }
+
+        public void setChessPiece(ChessPiece piece)
+        {
+            this.piece = piece;
+        }
+    }
+}
