3d4e2c766f96672b1ac958918fa6ae7378fec6e4
johncraik <jgcraik1@sheffield.ac.uk>
2022-03-11 15:58:07 +0000

    Added player 2 pieces

--- ChessSimple/Board.cs
+++ ChessSimple/Board.cs
@@ -44,6 +44,7 @@ namespace ChessSimple
             ChessPiece[,] board = new ChessPiece[8,8];
 
             //Create player 1 (ply1) pieces on the left side.
+            //Add back row pieces:
             int x = 0; int y = 0;
             int rook = 0; int knight = 0; int bishop = 0;
 
@@ -64,6 +65,34 @@ namespace ChessSimple
                 board[x, y] = new Pawn(new Point(x,y), ply1, i, true, false);
                 y++;
             }
+
+
+            //Create player 2 (ply2) pieces on the right side.
+            bool ply2 = !ply1;
+
+            //Add pawns:
+            x = 6; y = 0;
+            for (int i = 0; i < 8; i++)
+            {
+                board[x, y] = new Pawn(new Point(x, y), ply2, i, true, false);
+                y++;
+            }
+
+            //Add back row pieces:
+            x = 7; y = 0;
+            rook = 0; knight = 0; bishop = 0;
+
+            board[x, y] = new Rook(new Point(x, y), ply2, rook); rook++; y++;       //Rook
+            board[x, y] = new Knight(new Point(x, y), ply2, knight); knight++; y++; //Knight
+            board[x, y] = new Bishop(new Point(x, y), ply2, bishop); bishop++; y++; //Bishop
+            board[x, y] = new Queen(new Point(x, y), ply2, 0); y++;                 //Queen
+            board[x, y] = new King(new Point(x, y), ply2, 0, false, false); y++;    //King
+            board[x, y] = new Bishop(new Point(x, y), ply2, bishop); bishop++; y++; //Bishop
+            board[x, y] = new Knight(new Point(x, y), ply2, knight); knight++; y++; //Knight
+            board[x, y] = new Rook(new Point(x, y), ply2, rook); rook++; y++;       //Rook
+
+
+            
 
             return board;
         }
